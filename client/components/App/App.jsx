e493b451dc96338e8831ce5654e04d811cb5e633
Mike Abeln <mikeabeln>
2018-07-18 10:41:12 -0700

    setState refactor

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -7,17 +7,15 @@ import Footer from './../Footer/Footer.jsx'
 class App extends React.Component {
     constructor(props) {
         super(props)
-
         this.state = {
             path: window.location.pathname
         }
-
         this.props.history.listen(() => {
             if (this.state.path !== this.props.history.location.pathname) {
                 window.gtag('config', 'UA-122512534-1', {
                     page_path: this.props.history.location.pathname
                 })
-                this.state.path = this.props.history.location.pathname
+                this.setState({ path: this.props.history.location.pathname })
             }
         })
     }
