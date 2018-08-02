e9bd398fbb26df1ccf73445ed4c8c992db0342db
Mike Abeln <mikeabeln>
2018-08-02 16:54:21 -0700

    testing nav styles, more work to do

--- client/index.jsx
+++ client/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import 'normalize.css'
 import './index.scss'
 
-// import createBrowserHistory from 'history/createBrowserHistory'
-
 import Routes from './routes.jsx'
 
 export const renderApp = () => {
